a8cc7f0c96b0e59583b17e61d3d786862d98c1a4
Victor Montanez <victormontanez.student@careerdevs.com>
2019-04-08 21:38:56 +0000

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//This is the javascript code to the digial clock
+
+function clock() {
+  const fullDate = new Date();
+  let hours = fullDate.getHours();
+  let minutes = fullDate.getMinutes();
+  let seconds = fullDate.getSeconds();
+}
